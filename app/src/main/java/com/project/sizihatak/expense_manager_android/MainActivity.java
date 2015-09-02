fd499e32dd07d8515166af3fe475db5fc02f5fd7
Alyokhin Denis <da@anahoret.com>
2015-09-02 23:51:04 +0300

    Added main activity.

--- app/src/main/java/com/project/sizihatak/expense_manager_android/MainActivity.java
+++ app/src/main/java/com/project/sizihatak/expense_manager_android/MainActivity.java
@@ -1,9 +1,7 @@
 package com.project.sizihatak.expense_manager_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
 }
